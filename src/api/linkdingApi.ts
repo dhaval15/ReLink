66bc305b606d38b173f6a766cd33e87a6f47cd06
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-08-23 10:25:29 +0000

    fix: LinkdingApi changed to class, removed dependency on settings

--- src/api/linkdingApi.ts
+++ src/api/linkdingApi.ts
@@ -1,209 +1,161 @@
+import { Bookmark, BookmarkCheckResult, BookmarkResult } from "./types";
 
-import { getSettings } from "./settingsApi";
-import {Bookmark, BookmarkCheckResult, BookmarkResult}from "./types";
+class LinkdingApi {
+	instanceUrl: string;
+	token: string;
+	pageSize: number;
+	constructor(instanceUrl: string, token: string, pageSize: number = 10) {
+		this.instanceUrl = instanceUrl;
+		this.token = token;
+		this.pageSize = pageSize;
+	}
 
-const PAGE_SIZE = 10;
+	async getBookmarks(query?: string, page: number = 0): Promise<BookmarkResult> {
+		const url = new URL('api/bookmarks/', this.instanceUrl);
+		const params = {} as any;
+		if (query)
+			params.q = query;
+		params.limit = this.pageSize.toString();
+		params.offset = (page * this.pageSize).toString();
 
-const getBookmarks = async (query?: string, page: number = 0): Promise<BookmarkResult> => {
-    const settings = await getSettings();
+		const response = await fetch(url.toString(), {
+			method: 'GET',
+			headers: {
+				'Authorization': `Token ${this.token}`
+			}
+		});
 
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
-
-    // Add a delay to debug loading states
-    // await new Promise<void>(resolve => setTimeout(() => resolve(), 2000));
-
-    const url = new URL('api/bookmarks/', settings.instanceUrl);
-    const params = {} as any;
-
-    if (query)
-        params.q = query;
-
-    params.limit = PAGE_SIZE.toString();
-    params.offset = (page * PAGE_SIZE).toString();
-
-    const response = await fetch(url.toString(),{
-				method: 'GET',
-        headers: {
-            'Authorization': `Token ${settings.token}`
-        }
-    });
-
-    if (response.status !== 200)
-        throw new Error('Unable to load bookmarks');
+		if (response.status !== 200)
+			throw new Error('Unable to load bookmarks');
 
 		const data = await response.json();
 
-    return {
-        prevPage: data.previous ? page - 1 : undefined,
-        nextPage: data.next ? page + 1 : undefined,
-        results: data.results as Bookmark[]
-    };
-};
+		return {
+			prevPage: data.previous ? page - 1 : undefined,
+			nextPage: data.next ? page + 1 : undefined,
+			results: data.results as Bookmark[]
+		};
+	}
+	async getBookmark(id: number): Promise<Bookmark> {
+		const url = new URL(`api/bookmarks/${id}/`, this.instanceUrl);
 
-const getBookmark = async (id: number): Promise<Bookmark> => {
-    const settings = await getSettings();
+		const response = await fetch(url.toString(), {
+			method: 'GET',
+			headers: {
+				'Authorization': `Token ${this.token}`
+			}
+		});
 
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
-
-    const url = new URL(`api/bookmarks/${id}/`, settings.instanceUrl);
-
-    const response = await fetch(url.toString(),{
-				method: 'GET',
-        headers: {
-            'Authorization': `Token ${settings.token}`
-        }
-    });
-
-    if (response.status !== 200)
-        throw new Error('Unable to load bookmark');
+		if (response.status !== 200)
+			throw new Error('Unable to load bookmark');
 
 		const data = await response.json();
 
-    return data as Bookmark;
-};
+		return data as Bookmark;
+	}
 
-const checkUrl = async (urlToCheck: string): Promise<BookmarkCheckResult> => {
-    const settings = await getSettings();
-
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
-
-    const url = new URL(`api/bookmarks/check/`, settings.instanceUrl);
+	async checkUrl(urlToCheck: string): Promise<BookmarkCheckResult> {
+		const url = new URL(`api/bookmarks/check/`, this.instanceUrl);
 		const params = { url: urlToCheck };
 
-    const response = await fetch(url.toString(),{
-				method: 'GET',
-        headers: {
-            'Authorization': `Token ${settings.token}`
-        }
-    });
+		const response = await fetch(url.toString(), {
+			method: 'GET',
+			headers: {
+				'Authorization': `Token ${this.token}`
+			}
+		});
 
-    if (response.status !== 200)
-        throw new Error('Unable to check URL');
+		if (response.status !== 200)
+			throw new Error('Unable to check URL');
 
 		const data = await response.json();
 
-    return data as BookmarkCheckResult;
-};
+		return data as BookmarkCheckResult;
+	}
 
-const createBookmark = async (bookmark: Bookmark): Promise<void> => {
-    const settings = await getSettings();
+	async createBookmark(bookmark: Bookmark): Promise<void> {
+		const url = new URL(`api/bookmarks/`, this.instanceUrl);
 
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
+		await fetch(url.toString(), {
+			method: 'POST',
+			body: JSON.stringify({
+				url: bookmark.url,
+				title: bookmark.title,
+				description: bookmark.description,
+				unread: bookmark.unread,
+				tag_names: bookmark.tag_names
+			}),
+			headers: {
+				'Authorization': `Token ${this.token}`,
+				'Content-Type': 'application/json'
+			}
+		});
+	}
 
-    const url = new URL(`api/bookmarks/`, settings.instanceUrl);
+	async updateBookmark(bookmark: Bookmark): Promise<void> {
+		const url = new URL(`api/bookmarks/${bookmark.id}/`, this.instanceUrl);
 
-    await fetch(url.toString(),{
-				method: 'POST',
-        body: JSON.stringify({
-            url: bookmark.url,
-            title: bookmark.title,
-            description: bookmark.description,
-            unread: bookmark.unread,
-            tag_names: bookmark.tag_names
-        }),
-        headers: {
-            'Authorization': `Token ${settings.token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-};
+		await fetch(url.toString(), {
+			method: 'PATCH',
+			body: JSON.stringify({
+				title: bookmark.title,
+				description: bookmark.description,
+				unread: bookmark.unread,
+				tag_names: bookmark.tag_names
+			}),
+			headers: {
+				'Authorization': `Token ${this.token}`,
+				'Content-Type': 'application/json'
+			}
+		});
+	}
 
-const updateBookmark = async (bookmark: Bookmark): Promise<void> => {
-    const settings = await getSettings();
+	async updateBookmarkRead(id: number): Promise<void> {
+		const bookmark = await this.getBookmark(id);
+		const url = new URL(`api/bookmarks/${id}/`, this.instanceUrl);
 
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
+		await fetch(url.toString(), {
+			method: 'PATCH',
+			body: JSON.stringify({ unread: !bookmark.unread }),
+			headers: {
+				'Authorization': `Token ${this.token}`,
+				'Content-Type': 'application/json'
+			}
+		});
+	}
 
-    const url = new URL(`api/bookmarks/${bookmark.id}/`, settings.instanceUrl);
+	async deleteBookmark(id: number): Promise<void> {
+		const url = new URL(`api/bookmarks/${id}/`, this.instanceUrl);
 
-    await fetch(url.toString(),{
-				method: 'PATCH',
-        body: JSON.stringify({
-            title: bookmark.title,
-            description: bookmark.description,
-            unread: bookmark.unread,
-            tag_names: bookmark.tag_names
-        }),
-        headers: {
-            'Authorization': `Token ${settings.token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-};
+		await fetch(url.toString(), {
+			method: 'DELETE',
+			headers: {
+				'Authorization': `Token ${this.token}`,
+				'Content-Type': 'application/json'
+			}
+		});
+	}
 
-const updateBookmarkRead = async (id: number): Promise<void> => {
-    const settings = await getSettings();
-
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
-
-    const bookmark = await getBookmark(id);
-    const url = new URL(`api/bookmarks/${id}/`, settings.instanceUrl);
-
-    await fetch(url.toString(),{
-				method: 'PATCH',
-        body: JSON.stringify({ unread: !bookmark.unread }),
-        headers: {
-            'Authorization': `Token ${settings.token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-};
-
-const deleteBookmark = async (id: number): Promise<void> => {
-    const settings = await getSettings();
-
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
-    
-    const url = new URL(`api/bookmarks/${id}/`, settings.instanceUrl);
-    
-    await fetch(url.toString(),{
-				method: 'DELETE',
-        headers: {
-            'Authorization': `Token ${settings.token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-};
-
-const getTags = async (): Promise<string[]> => {
-    const settings = await getSettings();
-
-    if (settings.instanceUrl === undefined || settings.token === undefined)
-        throw new Error('Missing Linkdig settings. Please provide them from the Settings page.');
-
-    const url = new URL(`api/tags/`, settings.instanceUrl);
+	async getTags(): Promise<string[]> {
+		const url = new URL(`api/tags/`, this.instanceUrl);
 		const params = {
-				limit: '1000'
+			limit: '1000'
 		};
 
-    const response = await fetch( url.toString(),{
-				method: 'GET',
-        headers: {
-            'Authorization': `Token ${settings.token}`
-        }
-    });
+		const response = await fetch(url.toString(), {
+			method: 'GET',
+			headers: {
+				'Authorization': `Token ${this.token}`
+			}
+		});
 
-    if (response.status !== 200)
-        throw new Error('Unable to load tags');
+		if (response.status !== 200)
+			throw new Error('Unable to load tags');
 
 		const data = await response.json();
 
-    return data?.results?.map((tag: { name: string }) => tag.name) || [];
-};
+		return data?.results?.map((tag: { name: string }) => tag.name) || [];
+	}
+}
 
-export {
-    getBookmarks,
-    getBookmark,
-    checkUrl,
-    createBookmark,
-    updateBookmark,
-    updateBookmarkRead,
-    deleteBookmark,
-    getTags
-};
+export default LinkdingApi;
