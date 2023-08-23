4a68776fc5c5ee0890597ec0d38701216189d7f3
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-08-23 10:26:44 +0000

    ui: added api call to network

--- src/pages/LinksListPage.jsx
+++ src/pages/LinksListPage.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 import BookmarkTile from '../components/BookmarkTile';
+import LinkdingApi from '../api/linkdingApi';
 
+const settings = {
+	instanceUrl: 'INSTANCE_URL',
+	token: 'TOKEN',
+};
+
+const api = new LinkdingApi(settings.instanceUrl, settings.token);
 
 const LinksListPage = () => {
 	const [bookmarks, setBookmarks] = useState([]);
-	//TODO: Api Call
+	api.getBookmarks().then((result) => {
+		setBookmarks(result.results);
+	});
   return (
     <View style={styles.container}>
       <FlatList
