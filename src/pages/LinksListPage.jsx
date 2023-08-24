388e6f0ee4dea347ae20ba6b74ab27263d49724f
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-08-24 05:29:31 +0000

    fix: fixed re-rendering for each frame

--- src/pages/LinksListPage.jsx
+++ src/pages/LinksListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 import BookmarkTile from '../components/BookmarkTile';
 import LinkdingApi from '../api/linkdingApi';
@@ -12,26 +12,28 @@ const api = new LinkdingApi(settings.instanceUrl, settings.token);
 
 const LinksListPage = () => {
 	const [bookmarks, setBookmarks] = useState([]);
-	api.getBookmarks().then((result) => {
-		setBookmarks(result.results);
+	useEffect(() => {
+		api.getBookmarks().then((result) => {
+			setBookmarks(result.results);
+		});
 	});
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={[... bookmarks]}
-        keyExtractor={bookmark => bookmark.id.toString()}
-        renderItem={({ item }) => <BookmarkTile bookmark={item} />}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={[...bookmarks]}
+				keyExtractor={bookmark => bookmark.id.toString()}
+				renderItem={({ item }) => <BookmarkTile bookmark={item} />}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#eceff1',
-  },
+	container: {
+		flex: 1,
+		padding: 16,
+		backgroundColor: '#eceff1',
+	},
 });
 
 export default LinksListPage;
